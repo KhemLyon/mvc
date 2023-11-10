3f149e2a9de08ae0d00a58ccccab5fad4633c0fe
KhemLyon <khemcontact@gmail.com>
2023-11-10 16:26:13 +0100

    2

--- controllers/controller_admin_delete.php
+++ controllers/controller_admin_delete.php
@@ -1,12 +1,25 @@
 <?php
 // Si l'id est dans l'url on delete dans la table
-if (isset($_GET['id'])){
-    $id = intval( $_GET['id'] );
+// if (!isset($_SESSION['user'])) header("Location:?page=login");
+// Si l'id est dans l'url on delete dans la table
+// Si le delete dans la table a bien été effectué on unlink() le fichier
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
     $db = connectDB();
-    $sql = $db->prepare("DELETE FROM picture WHERE id=?");
+    // On fait une petite requète pour récupérer la source à supprimer
+    $sql = $db->prepare("SELECT src FROM picture WHERE id=?");
     $sql->execute([$id]);
-    unlink("./uploads/$newFile");
+    // Notez le fetchColumn() qui ne retourne qu'une seule info de la table
+    $srcToDelete = $sql->fetchColumn();
+    // On fait la suppression de la ligne dans la table
+    $sql = $db->prepare("DELETE FROM picture WHERE id=?");
+    // Si la requète de suppression a bien été effectuée
+    if ($sql->execute([$id])) {
+        // Si le fichier existe on le supprime avec unlink()
+        if (file_exists($srcToDelete)) {
+            unlink($srcToDelete);
+        }
+    }
     // Et on redirige sur l'admin_list
-    header("Location:?page=adminlist");
+    header("Location:?page=admin_list");
 }
-
